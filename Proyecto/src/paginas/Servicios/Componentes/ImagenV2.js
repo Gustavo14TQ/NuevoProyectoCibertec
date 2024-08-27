776c9d7bac39ca6dc04004cf599cf2aee24ea15b
AlSE10 <i202405977@cibertec.edu.pe>
2024-08-27 02:48:28 +0000

    realizando tercera pestaña

--- Proyecto/src/paginas/Servicios/Componentes/ImagenV2.js
+++ Proyecto/src/paginas/Servicios/Componentes/ImagenV2.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImagenVentana2 = ({ src, alt }) => {
   return (
     <div className="imagen-ventana2">
-      <img src={src} alt={alt} />
+      <img src={'https://www.damos.co/mediateca/files/planes_responsive.jpg'} alt={alt} />
     </div>
   );
 };
--- Proyecto/src/paginas/Servicios/ventanas/ventana2.js
+++ Proyecto/src/paginas/Servicios/ventanas/ventana2.js
@@ -1,8 +1,50 @@
 import { Fragment } from "react";
+import './ventana-2.css'
 function Ventana2(){
     return(
         <>
-        <h1>holaa</h1>
+        <header c>
+        <div class="div-1">
+            <p>
+                <b>EXPERTOS EN MEJORAR PÁGINAS WEB</b><br/> <br/>Ya sabes que tu sitio web es la columna vertebral de tu presencia digital. Y como tal, el mantenimiento web ayuda a reflejar el valor de tu marca. En concreto, estas son las ventajas de invertir en este proceso
+            </p>
+            <h3> "Con nosotros,<br/> tu sitio web estará siempre <br/>actualizado y seguro."</h3>
+            <button><a href="../APEXWEB_SOLUTION/Presupuesto.html">INGRESA TU PRESUPUESTO</a></button><button><a href="../Contactanos/Contactanos.html">CONTACTANOS</a></button>
+            
+        </div>
+    </header>
+    <div class="div-2">
+        <nav>
+            <img class="img-1"src="image/ventana3-2.webp"/>
+            <ul>SERVICIO DE MANTENIMIENTO WEB
+                <li>ACTUALIZACIONES</li>
+                <li>ELIMINACION DE PAGINAS ERROR</li>
+                <li>OPTIMIZACIÓN SEO</li>
+                <li>COPIAS DE SEGURIDAD</li>
+                <li>MONITORIZACIÓN</li>
+            </ul>
+        </nav>
+        <nav>
+            <img class="img-1" src="image/ventana3-4.webp"/>
+            <ul>TIPOS DE MANTENIMIENTO
+                <li>MANTENIMIENTO PREVENTIVO</li>
+                <li>MANTENIMIENTO CORRECTIVO</li>
+                <li>MANTENIMIENTO EVOLUTIVO</li>
+                <li>MANTENIMIENTOPERFECTIVO</li>
+                
+            </ul>
+        </nav>
+        <nav>
+            <img class="img-1" src="image/ventana3-3.webp"/>
+            <ul>SOPORTE WEB
+                <li>OPTIMIZACIÓN DE RENDIMIENTO</li>
+                <li>ANÁLISIS Y SEGIMIENTO</li>
+                <li>SEGURIDAD Y BACKUPS</li>
+                <li>ASESORAMIENTO Y CONSULTAS</li>
+                <li>FORMACION Y CAPACITACIÓN</li>
+            </ul>
+        </nav>
+    </div>
         </>
     )
 }
